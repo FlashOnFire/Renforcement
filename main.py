06cfa3e73aa8a1ebcf8c1d19cb747a55027d4013
FlashOnFire <flashonfire.dev@gmail.com>
2024-02-12 11:52:14 +0100

    ça marche c'est super cool

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ mat = np.array([[1, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 1, -2, 1],
                 [0, 0, 0, 0, 0, 0, 1], ])
 left = np.array([[296], [0], [0], [0], [0], [0], [0]])
-print(mat * left)
+x = np.linalg.solve(mat, left)
+
+print("x = ")
+print(x, end="\n\n")
+
+print("ax = ")
 # left = mat * [[t0],[t1],[t2],[t3],[t4],[t5],[t6]]
+
+print(np.fix(np.matmul(mat, x)))
